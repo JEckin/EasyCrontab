aa979e0c7e50938e330f341812fb9b77c217d721
root <root@2.j3ckin.de>
2019-08-28 08:17:47 +0200

    update

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ apt-get install nano
 cp /etc/crontab /etc/backup-crontab
 cp easycrontab.sh /bin/easycron
 cp easycrontab.sh /bin/ecrontab
+cp uninstall.sh /bin/uninstall-easycrontab
 chmod +x /bin/easycron
 chmod +x /bin/ecrontab
+chmod +x /bin/uninstall-easycrontab
 clear
 echo "Finished"
 echo "Type in \"easycron\" or \"ecrontab\""
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,48 @@
 #!/bin/bash
+main() {
+clear
+echo "Reset Crontab from Backup?"
+echo "1. Reset"
+echo "2. Reset and remove EasyCrontab"
+echo "3. Show Backup"
+echo "4. Just remove EasyCrontab"
+echo "5. Exit"
+read x
+case "$x" in
+1)
+cp /etc/backup-crontab /etc/crontab
+echo "Done"
+exit="true"
+;;
+2)
+mv /etc/backup-crontab /etc/crontab
 rm /bin/ecrontab
 rm /bin/easycron
+rm /bin/uninstall-easycrontab
+echo "Done"
+exit="true"
+;;
+3)
+nano /etc/backup-crontab
+;;
+4)
+rm /bin/ecrontab
+rm /bin/easycron
+rm /bin/uninstall-easycrontab
+echo "Done"
+exit="true"
+;;
+5)
+exit="true"
+;;
+esac
 echo "Finished"
+}
+
+
+exit="false"
+while [[ $exit == "false" ]]
+do
+main
+done
+
