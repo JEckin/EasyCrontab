a456b9fcf82bbeb4d713a6802ceb6dde69b0ae23
root <root@2.j3ckin.de>
2019-10-22 07:44:46 +0200

    update

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ apt-get install cron
 apt-get install nano
 
 cp /etc/crontab /etc/backup-crontab
+cp easycrontab.sh /bin/ecron
 cp easycrontab.sh /bin/easycron
-cp easycrontab.sh /bin/ecrontab
 cp uninstall.sh /bin/uninstall-easycrontab
 chmod +x /bin/easycron
 chmod +x /bin/ecrontab
 chmod +x /bin/uninstall-easycrontab
 clear
 echo "Finished"
-echo "Type in \"easycron\" or \"ecrontab\""
+echo "Type in \"ecron\" or \"easycron\""
